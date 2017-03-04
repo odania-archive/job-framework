32457bedee0a5d69bea09f5c6188168e1e1c591b
Mike Petersen <mike@odania-it.de>
2017-03-04 16:24:48 +0100

    Added favicon.ico

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -28,6 +28,24 @@ jobFramework.run(['$rootScope', function($rootScope) {
 	};
 }]);
 
+jobFramework.filter('orderObjectBy', function(){
+	return function(input, attribute) {
+		if (!angular.isObject(input)) return input;
+
+		var array = [];
+		for(var objectKey in input) {
+			array.push(input[objectKey]);
+		}
+
+		array.sort(function(a, b){
+			a = parseInt(a[attribute]);
+			b = parseInt(b[attribute]);
+			return a - b;
+		});
+		return array;
+	}
+});
+
 $(function () {
 	console.log("Set active");
 	$('a[href="' + this.location.pathname + '"]').parents('li,ul').addClass('active');
