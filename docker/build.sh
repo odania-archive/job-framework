a00a993afad8aee607ec35fececa2ae03b7b097b
Mike Petersen <mike@odania-it.de>
2017-04-11 01:12:23 +0200

    Updated linking

--- docker/build.sh
+++ docker/build.sh
@@ -3,5 +3,5 @@
 export GRADLE_OPTS=-Dgradle.user.home=/opt/job-framework/.gradle
 /opt/job-framework/gradlew assemble
 
-mv /opt/job-framework/build/libs/com-odaniait-job-framework-0.1.0.jar /opt/com-odaniait-job-framework-0.1.0.jar
-ln -sf /opt/com-odaniait-job-framework-0.1.0.jar /opt/com-odaniait-job-framework.jar
+mv /opt/job-framework/build/libs/com-odaniait-job-framework-0.2.0.jar /opt/com-odaniait-job-framework-0.2.0.jar
+ln -sf /opt/com-odaniait-job-framework-0.2.0.jar /opt/com-odaniait-job-framework.jar
