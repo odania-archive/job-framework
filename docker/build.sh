56c08ae9a33c804f7f1dee479d9b219f2f4a8403
Mike Petersen <mike@odania-it.de>
2017-04-13 12:56:11 +0200

    Additional checks in build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -4,4 +4,7 @@ export GRADLE_OPTS=-Dgradle.user.home=/opt/job-framework/.gradle
 /opt/job-framework/gradlew assemble
 
 mv /opt/job-framework/build/libs/com-odaniait-job-framework-*.jar /opt/com-odaniait-job-framework.jar
+[ ! -f "/opt/com-odaniait-job-framework.jar" ] && { echo "Error: /opt/com-odaniait-job-framework.jar does not exist!"; exit 2; }
+[ ! -s "/opt/com-odaniait-job-framework.jar" ] && { echo "Error: /opt/com-odaniait-job-framework.jar is empty!"; ls -lha /opt/; exit 2; }
+echo jobs:jobs /opt/com-odaniait-job-framework.jar
 
